e2feb43e58e8a571e47cfaba6865912f2b1467a0
JasonWachira <jason.kamwaro@strathmore.edu>
2025-04-24 17:59:16 +0300

    Added comments and formatted code better

--- Sum_List_Elems.py
+++ Sum_List_Elems.py
@@ -1,10 +1,12 @@
-
+# function that takes an array as an argument
 def addElements(arr):
+    # initialises a variable that stores the sum of the elements in the array
     total = 0
     for i in range(len(arr)):
+        # in each iteration, the value of total is increased by the value of arr[i]
         total += arr[i]
     return total
 
 
-my_list = [1,2,34,5,6,7,8,8]
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(addElements(my_list))
