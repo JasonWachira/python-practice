9c29c11e9ae5e3999d617364a95dc79f1522dbd0
JasonWachira <jason.kamwaro@strathmore.edu>
2025-04-24 18:18:39 +0300

    Question 3 commit

---
+++ loop_factorial.py
@@ -0,0 +1,11 @@
+def fact(num):
+    # initialises the factorial variable that stores the factorial on each iteration
+    factorial = 1
+    # loop starts at 1,2,3...num. it stops right before num+1 which is at num
+    for i in range(1, num + 1):
+        # shorthand for writing factorial = factorial * i which is the loop counter
+        factorial *= i
+    return factorial
+
+
+print(fact(0))
