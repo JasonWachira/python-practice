581ca8e2e725d6e16ede27b00bfb8192f63d8fad
JasonWachira <jason.kamwaro@strathmore.edu>
2025-04-26 08:05:31 +0300

    Modified loop_factorial.py

--- loop_factorial.py
+++ loop_factorial.py
@@ -1,4 +1,6 @@
 def fact(num):
+    if num < 0:
+        return "Cannot compute factorial of negative numbers"
     # initialises the factorial variable that stores the factorial on each iteration
     factorial = 1
     # loop starts at 1,2,3...num. it stops right before num+1 which is at num
@@ -11,4 +13,4 @@ def fact(num):
     return factorial
 
 
-print(fact(0)) # prints 120
+print(fact(5)) # prints 120
